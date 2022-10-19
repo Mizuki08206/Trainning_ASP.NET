a006425d95072732898acfc1b831714ee6685742
Mizuki08206 <114040034+Mizuki08206@users.noreply.github.com>
2022-10-19 19:10:48 +0900

    2022/10/19

---
+++ HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMvc.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World!!");
+        }
+        public IActionResult Index1()
+        {
+            return Content("こんにちは!!");
+        }
+    }
+}
