6b7f239716d718ebeb192d9b2bf91e14f33c66ab
D107-MKM30B4\ia <m-otomo@powersolutions.co.jp>
2022-10-21 16:59:47 +0900

    2022/10/21

---
+++ practice_ASP/Models/Unext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practice_ASP.Models
+{
+    public partial class Unext
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Genre { get; set; } = null!;
+        public string? Who { get; set; }
+    }
+}
---
+++ practice_ASP/Models/testdbContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace practice_ASP.Models
+{
+    public partial class testdbContext : DbContext
+    {
+        public testdbContext()
+        {
+        }
+
+        public testdbContext(DbContextOptions<testdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> People { get; set; } = null!;
+        public virtual DbSet<Unext> Unexts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=D107-MKM30B4\\SQLEXPRESS01;Database=testdb; Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("Person");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+            });
+
+            modelBuilder.Entity<Unext>(entity =>
+            {
+                entity.ToTable("Unext");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Genre)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("genre");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Who)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("who");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
